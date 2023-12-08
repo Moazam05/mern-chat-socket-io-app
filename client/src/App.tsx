db7ed29975ca73b633d688fcced6bd3874aca494
Moazam05 <salmanmoazam08@gmail.com>
2023-12-08 15:44:48 +0500

    client setup with MUI & redux toolkit & routes define

---
+++ client/src/App.tsx
@@ -0,0 +1,54 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PublicRoutes from "./routes/PublicRoutes";
+import SignUp from "./views/SignUp";
+import Login from "./views/Login";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+import Chat from "./views/Chat";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/signup"
+          element={
+            <PublicRoutes>
+              <SignUp />
+            </PublicRoutes>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoutes>
+              <Login />
+            </PublicRoutes>
+          }
+        />
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/chats"
+          element={
+            <ProtectedRoutes>
+              <Chat />
+            </ProtectedRoutes>
+          }
+        />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
