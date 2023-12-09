8c18b2d442de74392da7682953687af222aa21dc
Moazam05 <salmanmoazam08@gmail.com>
2023-12-09 18:21:14 +0500

    create chat api

---
+++ server/controllers/chatController.js
@@ -0,0 +1,51 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+
+exports.createChat = catchAsync(async (req, res, next) => {
+  // 1) Check if userId is provided || receiverId is provided
+  const { userId } = req.body;
+
+  if (!userId) {
+    return next(new AppError("Please provide a user id for chat", 400));
+  }
+
+  // 2) Check if chat already exists
+  let isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [{ users: { $in: [req.user._id] } }, { users: { $in: [userId] } }],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name email pic",
+  });
+
+  if (isChat.length > 0) {
+    // return chat
+    return res.status(200).json({
+      status: "success",
+      chat: isChat[0],
+    });
+  } else {
+    // Create new chat
+    const newChat = await Chat.create({
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    });
+
+    const FullChat = await Chat.findById(newChat._id).populate(
+      "users",
+      "-password"
+    );
+
+    return res.status(200).json({
+      status: "success",
+      chat: FullChat,
+    });
+  }
+});
---
+++ server/routes/chatRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const chatController = require("../controllers/chatController");
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+// CHAT ROUTES
+router.route("/").post(chatController.createChat);
+// router.route("/:id").get(chatController.getChat);
+
+// GROUP CHAT ROUTES
+// router.route("/group").post(chatController.createGroupChat);
+// router.route("/group/:id").put(chatController.renameGroupChat);
+// router.route("/group/:id/add").put(chatController.addToGroupChat);
+// router.route("/group/:id/remove").put(chatController.removeFromGroupChat);
+
+module.exports = router;
